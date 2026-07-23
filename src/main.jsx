43a47be3b9a03c81149e1ce7902d42f7028f0b5d
Ayomide Yerokun <Ayomideyerokun54@gmail.com>
2026-07-23 11:01:48 +0100

    fix: fixed error on UseAuth import and export

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import ThemeProvider from "./context/ThemeProvider.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <ThemeProvider>
+     <AuthProvider>
+       <ThemeProvider>
         <App />
       </ThemeProvider>
+     </AuthProvider>
     </GoogleOAuthProvider>
   </StrictMode>,
 );
